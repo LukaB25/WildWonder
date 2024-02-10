9e20c7fef81e6f96fe4b126205801f29209d45dd
Luka Black <luka.black20@gmail.com>
2024-02-10 19:37:07 +0000

    Add Comment model. Add messages to posts and comments on admin page

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = ((0, 'Published'), (1, 'Draft'), (2, 'Deleted'))
+COMMENT_STATUS = ((0, 'Published'), (1, 'Deleted'))
+
 
 # Create your models here.
 class Post(models.Model):
@@ -20,3 +22,26 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.location_name} | Written by: {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=200, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=True)
+    status = models.IntegerField(choices=COMMENT_STATUS, default=0)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: {self.body} | Written by: {self.author} | On a post: {self.post.location_name}"
+
